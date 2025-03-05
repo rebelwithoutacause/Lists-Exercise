be85e608c14b46629eeec0854bb084da2894f4d9
Teodor Kostov <148477548+rebelwithoutacause@users.noreply.github.com>
2025-03-05 09:00:25 +0200

    Add files via upload

---
+++ 01.ChangeList/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ListManipulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            string command;
+            while ((command = Console.ReadLine()) != "end")
+            {
+                string[] tokens = command.Split();
+                string action = tokens[0];
+
+                if (action == "Delete")
+                {
+                    int element = int.Parse(tokens[1]);
+                    numbers.RemoveAll(num => num == element);
+                }
+                else if (action == "Insert")
+                {
+                    int element = int.Parse(tokens[1]);
+                    int position = int.Parse(tokens[2]);
+                    if (position >= 0 && position <= numbers.Count)
+                    {
+                        numbers.Insert(position, element);
+                    }
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
